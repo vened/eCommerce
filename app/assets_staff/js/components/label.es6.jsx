f6e9bd546b2d542f8cca5b8c8ec93793b3e88d3c
Maksim Stolbukhin <maxstbn@gmail.com>
2016-03-23 21:29:46 +0300

    модель категорий

--- app/assets_staff/js/components/label.es6.jsx
+++ app/assets_staff/js/components/label.es6.jsx
@@ -1,12 +1,26 @@
 var Button = ReactBootstrap.Button;
 var OverlayTrigger = ReactBootstrap.OverlayTrigger;
 var Popover = ReactBootstrap.Popover;
+var SplitButton = ReactBootstrap.SplitButton;
+var MenuItem = ReactBootstrap.MenuItem;
 
 class Post extends React.Component {
+    constructor(props){
+        super(props)
+        console.log(this);
+    }
+
+
     render () {
         return (
             <div>
-                <div>Label</div>
+                <SplitButton bsStyle='Primary' title='Каталог товаров'>
+                    <MenuItem eventKey="1">Action</MenuItem>
+                    <MenuItem eventKey="2">Another action</MenuItem>
+                    <MenuItem eventKey="3">Something else here</MenuItem>
+                    <MenuItem divider/>
+                    <MenuItem eventKey="4">Separated link</MenuItem>
+                </SplitButton>
                 <OverlayTrigger trigger="hover" placement="bottom" overlay={<Popover title="Popover bottom"><strong>Holy guacamole!</strong> Check this info.</Popover>}>
                     <Button bsStyle="default">Hover</Button>
                 </OverlayTrigger>
