976532b4db29b5050d9c9856a15ff6ac07e13225
Maksim Stolbukhin <maxstbn@gmail.com>
2016-04-28 00:45:05 +0300

    обновление товаров из списка

--- app/assets_staff/js/components/products/products.js
+++ app/assets_staff/js/components/products/products.js
@@ -1,9 +1,10 @@
-appComponents.controller('ProductsIndexController', function ($scope, Product) {
+appComponents.controller('ProductsIndexController', function ($rootScope, $scope, $mdToast, Product) {
 
     $scope.updateProduct = function (product_id, data) {
         Product.put(product_id, data)
             .then(function (res) {
                 if (res.status == 200) {
+                    showCustomToast('Продукт ' + product_id + ' обновлен!')
                     $scope['staff_product_hit_' + product_id] = res.data.hit;
                     $scope['staff_product_new_' + product_id] = res.data.new;
                     $scope['staff_product_action_' + product_id] = res.data.action;
@@ -11,6 +12,18 @@ appComponents.controller('ProductsIndexController', function ($scope, Product) {
                 }
             })
     }
+
+
+    function showCustomToast (text) {
+
+
+        var toast = $mdToast.simple()
+            .textContent(text)
+            .action('Закрыть')
+            .hideDelay(1500)
+            .position('bottom right');
+        $mdToast.show(toast);
+    };
 
 
 });
