13eb82893a4f7e960c566f60c13d485ed9e5f1fc
Maksim Stolbukhin <maxstbn@gmail.com>
2016-04-27 22:35:23 +0300

    seeds

--- db/seeds/seed_products.rb
+++ db/seeds/seed_products.rb
@@ -1,16 +1,18 @@
-200.times do |i|
+2000.times do |i|
+  rand = Random.new
   name = Faker::Commerce.product_name
   sku  = Faker::Code.isbn
   p "create product - #{name}"
   Product.create(
-      name:       name,
-      meta_title: name,
-      sku:        sku,
-      mini_desc:  Faker::Lorem.paragraph,
-      desc:       Faker::Lorem.paragraph,
-      meta_title: name,
-      meta_desc:  name,
-      meta_key:   name,
-      active:     true
+      name:        name,
+      meta_title:  name,
+      sku:         sku,
+      mini_desc:   Faker::Lorem.paragraph,
+      desc:        Faker::Lorem.paragraph,
+      meta_title:  name,
+      meta_desc:   name,
+      meta_key:    name,
+      active:      true,
+      category_id: rand.rand(1..15),
   )
 end
